716afa7355299571e8f93469847d0b3503efb73c
Mark Perkins <mark@allmarkedup.com>
2016-07-10 12:12:32 +0100

    Cleanup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Fractal Demo
 
-This is a demo of the [Fractal](http://github.com/frctl/fractal) web interface.
+!![WIP]!!
+
+This is a demo of an example [Fractal](http://github.com/frctl/fractal) project.
 
 This demo site is _not_ intended to be the simplest possible example of how you can use Fractal. Instead the focus is on demonstrating some of the slightly more advanced features that Fractal offers.
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,59 @@
 'use strict';
 
 const gulp     = require('gulp');
-// const sass     = require('gulp-sass');
-// const sassGlob = require('gulp-sass-glob');
+const sass     = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob');
 const del      = require('del');
 
-const fractal  = require('./fractal.js');
-const console = fractal.cli.console;
+const fractal  = require('./fractal.js'); // import the Fractal instance configured in the fractal.js file
+const logger = fractal.cli.console;      // make use of Fractal's console object for logging
+
+/*
+ * An example of a Gulp task that starts a Fractal development server.
+ */
 
 gulp.task('fractal:start', function(){
     const server = fractal.web.server({
         sync: true
     });
-    server.on('error', err => console.error(err.message));
+    server.on('error', err => logger.error(err.message));
     return server.start().then(() => {
-        console.success(`Fractal server is now running at ${server.urls.sync.local}`);
+        logger.success(`Fractal server is now running at ${server.urls.sync.local}`);
     });
 });
+
+/*
+ * An example of a Gulp task that to run a static export of the web UI.
+ */
 
 gulp.task('fractal:build', function(){
     const builder = fractal.web.builder();
-    builder.on('progress', (completed, total) => console.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => console.error(err.message));
+    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
+    builder.on('error', err => logger.error(err.message));
     return builder.build().then(() => {
-        console.success('Fractal build completed!');
+        logger.success('Fractal build completed!');
     });
 });
 
-// gulp.task('css:process', function() {
-//   return gulp.src('assets/css/main.scss')
-//     .pipe(sassGlob())
-//     .pipe(sass())
-//     .pipe(gulp.dest('public/css'));
-// });
-//
-// gulp.task('css:clean', function() {
-//     return del(['public/css']);
-// });
-//
-// gulp.task('css:watch', function () {
-//     gulp.watch([
-//         'assets/css/**/*.scss',
-//         'components/**/*.scss'
-//     ], gulp.series('css'));
-// });
-//
-// gulp.task('css', gulp.series('css:clean', 'css:process'));
-//
-// gulp.task('default', gulp.parallel('css'));
-// gulp.task('watch', gulp.parallel('css:watch'));
+gulp.task('css:process', function() {
+  return gulp.src('assets/css/main.scss')
+    .pipe(sassGlob())
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('css:clean', function() {
+    return del(['public/css']);
+});
+
+gulp.task('css:watch', function () {
+    gulp.watch([
+        'assets/css/**/*.scss',
+        'components/**/*.scss'
+    ], gulp.series('css'));
+});
+
+gulp.task('css', gulp.series('css:clean', 'css:process'));
+
+gulp.task('default', gulp.parallel('css'));
+gulp.task('watch', gulp.parallel('css:watch'));
